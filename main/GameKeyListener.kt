805ddb8f2e150cd540446161cb9c4cad9528f1f7
Damien Dervishi <DervishiD01@gmail.com>
2019-01-17 14:16:35 +0100

    Added empty listener files

---
+++ main/GameKeyListener.kt
@@ -0,0 +1,20 @@
+package main
+
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+
+class GameKeyListener : KeyListener {
+
+    public override fun keyPressed(e: KeyEvent?) {
+        //TODO
+    }
+
+    public override fun keyReleased(e: KeyEvent?) {
+        //TODO
+    }
+
+    public override fun keyTyped(e: KeyEvent?) {
+        //TODO
+    }
+
+}
---
+++ main/GameMouseListener.kt
@@ -0,0 +1,28 @@
+package main
+
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+
+class GameMouseListener : MouseListener {
+
+    public override fun mouseClicked(e: MouseEvent?) {
+        //TODO
+    }
+
+    public override fun mouseEntered(e: MouseEvent?) {
+        //TODO
+    }
+
+    public override fun mouseExited(e: MouseEvent?) {
+        //TODO
+    }
+
+    public override fun mousePressed(e: MouseEvent?) {
+        //TODO
+    }
+
+    public override fun mouseReleased(e: MouseEvent?) {
+        //TODO
+    }
+
+}
