edd830420fa9415ea51c27fee387b479b5069cae
Damien Dervishi <DervishiD01@gmail.com>
2019-01-17 15:01:23 +0100

    Added part of the geometric stuff

--- geometry/Point.kt
+++ geometry/Point.kt
@@ -1,5 +1,28 @@
 package geometry
 
 class Point {
-    
+
+    private var x : Double
+    private var y : Double
+
+    constructor(x : Double, y : Double){
+        this.x = x
+        this.y = y
+    }
+    constructor(x : Int, y : Int) : this(x.toDouble(), y.toDouble())
+    constructor() : this(0, 0)
+    constructor(p : Point) : this(p.x, p.y)
+    constructor(v : Vector) : this(v.x(), v.y())
+
+    public fun x() : Double = x
+    public fun y() : Double = y
+
+    public fun intx() : Int = x.toInt()
+    public fun inty() : Int = y.toInt()
+
+    /*
+    * Addition of a Vector to a Point, corresponds to moving the Point along the Vector.
+    * */
+    public operator fun plus(v : Vector) : Point = Point(x + v.x(), y + v.y())
+
 }
--- geometry/Vector.kt
+++ geometry/Vector.kt
@@ -1,4 +1,48 @@
 package geometry
 
 class Vector {
+
+    private var x : Double
+    private var y : Double
+
+    constructor(x : Double, y : Double){
+        this.x = x
+        this.y = y
+    }
+    constructor(x : Int, y : Int) : this(x.toDouble(), y.toDouble())
+    constructor() : this(0, 0)
+    constructor(p : Point) : this(p.x(), p.y())
+    constructor(v : Vector) : this(v.x, v.y)
+
+    public fun x() : Double = x
+    public fun y() : Double = y
+
+    /*
+    * Change the sign of x and y.
+    * */
+    public fun oppose(){
+        x = -x
+        y = -y
+    }
+
+    /*
+    * Return the opposite of this Vector.
+    * */
+    public fun opposed() : Vector = Vector(-x, -y)
+
+    /*
+    * Vector addition.
+    * */
+    public operator fun plus(v : Vector) : Vector = Vector(x + v.x, y + v.y)
+
+    /*
+    * Vector subtraction.
+    * */
+    public operator fun minus(v : Vector) : Vector = Vector(x - v.x, y - v.y)
+
+    /*
+    * Dot product.
+    * */
+    public operator fun times(v : Vector) : Double = x * v.x + y * v.y
+
 }
