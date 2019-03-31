d68761abda85f5e1ac895e3b80b03f773210ae7f
Damien Dervishi <DervishiD01@gmail.com>
2019-03-31 22:30:08 +0200

    Commented DisplayerContainer

--- display/DisplayerContainer.kt
+++ display/DisplayerContainer.kt
@@ -3,33 +3,165 @@ package display
 import geometry.Point
 import java.awt.Graphics
 
+/**
+ * A simple Displayer Container, nothing special.
+ * @see Displayer
+ * @see AbstractDisplayerContainer
+ */
 class DisplayerContainer : AbstractDisplayerContainer {
 
     override val parts: MutableCollection<Displayer> = mutableListOf()
 
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param p The Point at the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(p : Point, width : Int, height : Int, displayers : Collection<Displayer>) : super(p){
         w = width
         h = height
         parts.addAll(displayers)
     }
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Int, y : Int, width : Int, height : Int, displayers : Collection<Displayer>) : this(Point(x, y), width, height, displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Double, y : Int, width : Int, height : Int, displayers : Collection<Displayer>) : this(Point(x, y), width, height, displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Int, y : Double, width : Int, height : Int, displayers : Collection<Displayer>) : this(Point(x, y), width, height, displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Double, y : Double, width : Int, height : Int, displayers : Collection<Displayer>) : this(Point(x, y), width, height, displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param p The Point at the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(p : Point, width : Int, height : Int, vararg displayers : Displayer) : super(p){
         w = width
         h = height
         parts.addAll(displayers)
     }
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Int, y : Int, width : Int, height : Int, vararg displayers: Displayer) : this(Point(x, y), width, height, *displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Double, y : Int, width : Int, height : Int, vararg displayers: Displayer) : this(Point(x, y), width, height, *displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Int, y : Double, width : Int, height : Int, vararg displayers: Displayer) : this(Point(x, y), width, height, *displayers)
+
+    /**
+     * Constructs a DisplayerContainer with the given parameters.
+     * @param x The x coordinate of the center of this DisplayerContainer.
+     * @param y The y coordinate of the center of this DisplayerContainer.
+     * @param width The width of this DisplayerContainer.
+     * @param height The height of this DisplayerContainer.
+     * @param displayers The Displayers contained in this DisplayerContainer.
+     * @see Point
+     * @see Displayer
+     * @see addDisplayers
+     */
     constructor(x : Double, y : Double, width : Int, height : Int, vararg displayers: Displayer) : this(Point(x, y), width, height, *displayers)
 
+    /**
+     * Adds the given displayers to this DisplayerContainer.
+     * @param displayers The added Displayers.
+     * @see Displayer
+     */
     fun addDisplayers(vararg displayers : Displayer){
         parts.addAll(displayers)
     }
 
+    /**
+     * Adds the given displayers to this DisplayerContainer.
+     * @param displayers The added Displayers.
+     * @see Displayer
+     */
     infix fun addDisplayers(displayers : Collection<Displayer>){
         parts.addAll(displayers)
     }
--- main/debugFile.kt
+++ main/debugFile.kt
@@ -71,7 +71,7 @@ internal fun debugText(){
 }
 
 internal fun debugCollectionCast(){
-    var a : ArrayList<MutableList<Int>> = arrayListOf(mutableListOf(1, 2), mutableListOf(3, 4))
-    var b = a.toCollectionOf<List<Int>, MutableList<Int>>()
+    val a : ArrayList<MutableList<Int>> = arrayListOf(mutableListOf(1, 2), mutableListOf(3, 4))
+    val b = a.toCollectionOf<List<Int>, MutableList<Int>>()
     b.print()
 }
